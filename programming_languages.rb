6aa950937ac37e46a2ce98ebcbc6da61d29bd5dd
Alan Golman <golman.alan@gmail.com>
2018-05-30 17:31:24 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,13 +3,10 @@ def reformat_languages(languages)
   languages.each do |kind,language_name|
     language_name.each do |language, type|
       if new_hash.has_key?(language)
-        #if new_hash already has the language, just merge the kind array,
-        # and the language that new_hash has
         style_array = (new_hash[language][:style] + [kind]).uniq
         style_hash = {:style => style_array}
         new_hash[language] = type.merge(style_hash)
       else
-        #otherwise, just use the kind in an array
         style_array = [kind]
         style_hash = {:style => style_array}
         new_hash[language] = type.merge(style_hash)
